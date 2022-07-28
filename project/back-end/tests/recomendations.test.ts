7c47bd3e4c2bb6fe8abf4afeae012b6d7760baae
Matheus-daSilva <matheus2001silva@live.com>
2022-07-27 23:53:54 -0300

    add: /recommendations route test

--- project/back-end/tests/recomendations.test.ts
+++ project/back-end/tests/recomendations.test.ts
@@ -1,14 +1,26 @@
 import {prisma} from "../src/database.js"
+import { faker } from '@faker-js/faker'
 import supertest from "supertest"
 import app from "../src/app.js"
 
 beforeEach(async () => {
-    await prisma.$executeRaw`TRUNCATE TABLE recomendations;`;
+    await prisma.$executeRaw`TRUNCATE TABLE recommendations;`;
 });
+
+const body = {
+    name: faker.name,
+    youtubeLink: `https://www.youtube.com/${faker.name}`
+}
+
 
 describe("POST /recomendations", () => {
 
     it("postagem de uma recomendação de vídeo", async () => {
-        
+        const respo = await supertest(app).post("/recommendations").send(body)
+        expect(respo.statusCode).toBe(201)
     })
 })
+
+afterAll(async () => {
+    await prisma.$disconnect();
+});
