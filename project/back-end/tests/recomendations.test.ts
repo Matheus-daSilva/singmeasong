39dea27921237f697c9d29ba6bd7b2185a033b55
Matheus-daSilva <matheus2001silva@live.com>
2022-07-28 21:17:36 -0300

    add: get recomendation id by the factroy and the upvote test

--- project/back-end/tests/recomendations.test.ts
+++ project/back-end/tests/recomendations.test.ts
@@ -1,7 +1,8 @@
-import {prisma} from "../src/database.js"
+import { prisma } from "../src/database.js"
 import { faker } from '@faker-js/faker'
 import supertest from "supertest"
 import app from "../src/app.js"
+import { getPostIdFactory } from "./factory/getIdFactory.js";
 
 beforeEach(async () => {
     await prisma.$executeRaw`TRUNCATE TABLE recommendations;`;
@@ -25,6 +26,16 @@ describe("POST /recomendations", () => {
         await supertest(app).post("/recommendations").send(body2)
         const respo = await supertest(app).post("/recommendations").send(body2)
         expect(respo.statusCode).toBe(409)
+    })
+})
+
+describe("POST /recommendations/:id/upvote", () => {
+    
+    it("trying to add a vote to the video", async () => {
+        await supertest(app).post("/recommendations").send(body)
+        const respo = await getPostIdFactory(body.name)
+        const respo2 = await supertest(app).post(`/recommendations/${respo.id}/upvote`).send()
+        expect(respo2.statusCode).toBe(200)
     })
 })
 
