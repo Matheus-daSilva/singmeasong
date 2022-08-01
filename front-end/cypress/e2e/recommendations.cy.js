8d6b06133d07080a6616d0de395987acfabcf400
Matheus-daSilva <matheus2001silva@live.com>
2022-08-01 13:19:52 -0300

    add:recommendations cypress tests, posting a recommeendation and the navigation to the top page

---
+++ front-end/cypress/e2e/recommendations.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+import { faker } from '@faker-js/faker'
+
+describe("user behavior", () => {
+
+    it("user should be able to post a recommendation", async () => {
+
+        const body = {
+            name: faker.name.findName(),
+            youtubeLink: "https://www.youtube.com/watch?v=CBF25QcaC_g&list=RDCBF25QcaC_g&start_radio=1"
+        }
+
+        cy.visit("http://localhost:3000/")
+        cy.get("#name").type(body.name)
+        cy.get("#link").type(body.youtubeLink)
+
+        cy.get("#button").click()
+
+    })
+
+    it("user should see top recommendations", async () => {
+
+        cy.visit("http://localhost:3000/")
+        cy.get("#top").click()
+
+        cy.url().should("equal", "http://localhost:3000/top")
+    })
+})
